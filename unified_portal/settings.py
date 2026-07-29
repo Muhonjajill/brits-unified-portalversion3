afd552b0bb72c736208aa76e4f08f44faeb52b78
Goddy21 <godblessodhiambo@gmail.com>
2026-07-29 14:27:41 +0300

    logo clickable

--- unified_portal/settings.py
+++ unified_portal/settings.py
@@ -128,7 +128,8 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'pre_dashboards'
 LOGOUT_REDIRECT_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #EMAIL_HOST = 'smtp.zoho.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
